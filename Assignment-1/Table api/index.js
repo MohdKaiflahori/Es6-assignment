217bb0e27d0d80dd6cd8ead6d541cab1215e4bb0
MohdkaifLahori <kaif.lahori@habilelabs.ip>
2023-02-06 18:02:30 +0530

    Folders

--- Assignment-1/Table api/index.js
+++ Assignment-1/Table api/index.js
@@ -1,10 +1,12 @@
-axios.get("https://jsonplaceholder.typicode.com/albums")
+axios
+  .get("https://jsonplaceholder.typicode.com/albums")
   .then((res) => res.data)
   .then((data) =>
     data.map((v) => {
       let tbody = document.getElementById("table");
       tbody.innerHTML += `<tr> <td>${v.userId}</td> <td> ${v.id}</td> <td>${v.title}</td>`;
     })
-  ).catch((err)=>{
+  )
+  .catch((err) => {
     console.log(err);
   });
